78c67ae6a284cd7be4fb150087b22422b18454f1
Thinh <dtthinhit.54@gmail.com>
2024-09-09 19:56:10 +0700

    2 day

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
 const Cart = () => {
+  const {cartItems, food_list, removeFromCart} = useContext(StoreContext)
   return (
-    <div>
-      
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantily</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item,index)=>{
+          if(cartItems[item._id>0]){
+            return(
+              <div className='cart-items-title cart-items-item'>
+                <p>{item.name}</p>
+              </div>
+            )
+          }
+        })}
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,13 +3,15 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 const Home = () => {
     const [category,setCategory]= useState('All');
   return (
     <div>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
-      <FoodDisplay/>
+      <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
